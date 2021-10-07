623e6f270d12c5afe7c9bc73cb10cae089ab3de1
Mimi <my.ntk207691@sis.hust.edu.vn>
2021-10-07 20:49:05 +0700

    Mei

---
+++ ConsoleApplication3/Program.cs
@@ -0,0 +1,13 @@
+﻿
+using System;
+
+namespace ConsoleApplication3
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello Meimei");
+        }
+    }
+}
